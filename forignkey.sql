1d3477f28efe4d4306611f4a85a0fa15d7401a36
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-08-08 11:28:30 +0530

    new update

---
+++ forignkey.sql
@@ -0,0 +1,161 @@
+CREATE TABLE supplier_groups(
+    group_id NUMBER,
+    group_name VARCHAR2(255) NOT NULL,
+    PRIMARY KEY (group_id)  
+);
+
+CREATE TABLE suppliers (
+    supplier_id NUMBER,
+    supplier_name VARCHAR2(255) NOT NULL,
+    group_id NUMBER NOT NULL,
+    PRIMARY KEY(supplier_id),
+    FOREIGN KEY(group_id) 
+    REFERENCES supplier_groups(group_id)
+);
+
+INSERT INTO supplier_groups(group_id ,group_name) 
+VALUES(1,'One-time Supplier');
+INSERT INTO supplier_groups(group_id ,group_name) 
+VALUES(2,'Third-party Supplier');
+INSERT INTO supplier_groups(group_id ,group_name)
+VALUES(3,'Inter-co Supplier');
+
+SELECT * FROM supplier_groups;
+
+INSERT INTO suppliers(supplier_id,supplier_name, group_id)
+VALUES(1,'Toshiba',1);
+INSERT INTO suppliers(supplier_id,supplier_name, group_id)
+VALUES(2'WD',4);
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10) not null,
+  CONSTRAINT fk_supplier
+    FOREIGN KEY (supplier_id)
+    REFERENCES supplier(supplier_id)
+);
+----------------------------------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id, supplier_name)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  CONSTRAINT fk_supplier_comp
+    FOREIGN KEY (supplier_id, supplier_name)
+    REFERENCES supplier(supplier_id, supplier_name)
+);
+
+-----------------------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10) not null,
+  CONSTRAINT fk_supplier
+    FOREIGN KEY (supplier_id)
+    REFERENCES supplier(supplier_id)
+    ON DELETE CASCADE
+);
+-----------------------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id, supplier_name)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  CONSTRAINT fk_supplier_comp
+    FOREIGN KEY (supplier_id, supplier_name)
+    REFERENCES supplier(supplier_id, supplier_name)
+    ON DELETE CASCADE
+);
+----------------------------------------------------------
+ALTER TABLE products
+ADD CONSTRAINT fk_supplier
+  FOREIGN KEY (supplier_id)
+  REFERENCES supplier(supplier_id)
+  ON DELETE CASCADE;
+
+
+ALTER TABLE products
+ADD CONSTRAINT fk_supplier
+  FOREIGN KEY (supplier_id, supplier_name)
+  REFERENCES supplier(supplier_id, supplier_name)
+  ON DELETE CASCADE;
+---------------------------------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10),
+  CONSTRAINT fk_supplier
+    FOREIGN KEY (supplier_id)
+    REFERENCES supplier(supplier_id)
+    ON DELETE SET NULL
+);
+----------------------------------
+CREATE TABLE supplier
+( supplier_id numeric(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_pk PRIMARY KEY (supplier_id, supplier_name)
+);
+
+CREATE TABLE products
+( product_id numeric(10) not null,
+  supplier_id numeric(10),
+  supplier_name varchar2(50),
+  CONSTRAINT fk_supplier_comp
+    FOREIGN KEY (supplier_id, supplier_name)
+    REFERENCES supplier(supplier_id, supplier_name)
+    ON DELETE SET NULL
+);
+-------------------------------------
+
+ALTER TABLE products
+ADD CONSTRAINT fk_supplier
+  FOREIGN KEY (supplier_id)
+  REFERENCES supplier(supplier_id)
+  ON DELETE SET NULL;
+
+ALTER TABLE products
+ADD CONSTRAINT fk_supplier
+  FOREIGN KEY (supplier_id, supplier_name)
+  REFERENCES supplier(supplier_id, supplier_name)
+  ON DELETE SET NULL;
